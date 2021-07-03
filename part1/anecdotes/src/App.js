a497cd579edad6e9479cc1205c29506dc6e39c78
Ilya Povarov <ilya.povaroff@gmail.com>
2021-07-03 11:00:07 +0300

    1.14*: anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,19 +13,31 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const [best, setBest] = useState([anecdotes.indexOf(anecdotes[Math.floor(Math.random()*anecdotes.length)])])
 
   const handleVote = () => {
     const votesCopy = [ ...votes ];
+    const bestAnecdotes = []
     votesCopy[selected] += 1
     setVotes(votesCopy)
     console.log(votes)
+    votes.forEach(el => {
+      if(el === Math.max.apply(null, votes)) {
+        bestAnecdotes.push(votes.indexOf(el))
+      }
+    })
+    setBest(bestAnecdotes[Math.floor(Math.random()*bestAnecdotes.length)])
+    console.log('best', bestAnecdotes)
   }
 
   return (
     <div>
+      <h2>anecdote of the day</h2>
       <Anecdote anecdote={anecdotes[selected]} votes={votes[selected]}/>
       <button onClick={handleVote}>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))}>new anecdote</button>
+      <h2>anecdote with most votes</h2>
+      <Anecdote anecdote={anecdotes[best]} votes={votes[best]} />
     </div>
   )
 }
