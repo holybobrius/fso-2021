94720e737b3c7cf9bf56d873273d267ea303c8d8
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2021-07-16 16:08:14 +0300

    2.13*: Data for countries, step2

--- part2/data-for-countries/src/components/Countries.js
+++ part2/data-for-countries/src/components/Countries.js
@@ -4,7 +4,7 @@ import Country from './Country'
 const Countries = ({ data }) => {
     return (
         <div>
-            {data.length !== 1 ? data.map(country => <p key={country.name}>{country.name}</p>) : <Country country={data[0]}/>}
+            {data.length !== 1 ? data.map(country => <Country key={country.name} country={country}/>) : <Country country={data[0]}/>}
         </div>
     )
 }
--- part2/data-for-countries/src/components/Country.js
+++ part2/data-for-countries/src/components/Country.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Country = ( { country }) => {
+    const [show, setShow] = useState(false)
     const getLanguages = () => (
         country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)
     )
+
+    const handleClick = () => {
+        setShow(!show)
+    }
     return (
         <div>
-            <h2>{country.name}</h2>
-            <p>Capital: {country.capital}</p>
-            <p>Population: {country.population}</p>
-            <h3>Languages:</h3>
-            <ul>
-                {getLanguages()}
-            </ul>
-            <img width='100px' height='100px' src={country.flag}/>
+            {
+                show ? 
+                    <div>
+                        <h2>{country.name}</h2>
+                        <p>Capital: {country.capital}</p>
+                        <p>Population: {country.population}</p>
+                        <h3>Languages:</h3>
+                        <ul>
+                            {getLanguages()}
+                        </ul>
+                        <img width='100px' height='100px' src={country.flag}/>
+                    </div>
+                : <p>{country.name}</p> 
+            }
+            <button onClick={handleClick}>show</button>
         </div>
     )
 }
