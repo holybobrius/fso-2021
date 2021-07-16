f0e91b584834c35ab9867e748f0607c9c3c26ee9
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2021-07-16 17:27:44 +0300

    2.14*: Data for countries, step3

--- part2/data-for-countries/src/components/Country.js
+++ part2/data-for-countries/src/components/Country.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ( { country }) => {
     const [show, setShow] = useState(false)
+    const [weather, setWeather] = useState()
     const getLanguages = () => (
         country.languages.map(language => <li key={language.iso639_1}>{language.name}</li>)
     )
+
+    const api_key = process.env.REACT_APP_API_KEY
+    const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+    console.log(api_key)
+    console.log(url)
+    useEffect(() => axios.get(url).then(res => setWeather(res.data)), [])
+    
 
     const handleClick = () => {
         setShow(!show)
@@ -22,6 +31,10 @@ const Country = ( { country }) => {
                             {getLanguages()}
                         </ul>
                         <img width='100px' height='100px' src={country.flag}/>
+                        <h3>Weather in {country.capital}:</h3>
+                        <p>temperature: {weather.current.temperature} Celcium</p>
+                        <img width='70px' height='70px' src={weather.current.weather_icons[0]}/>
+                        <p>wind: {weather.current.wind_speed} mph direction {weather.current.wind_dir}</p>
                     </div>
                 : <p>{country.name}</p> 
             }
