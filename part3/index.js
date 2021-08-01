b297081e1f0d0e69ecf061586ac3677975bc8819
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-01 11:51:57 +0300

    3.9 phonebook backend step9

--- part3/index.js
+++ part3/index.js
@@ -1,12 +1,16 @@
 const { response, request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+
 
 
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
     { 
