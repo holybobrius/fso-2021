820ab3f2155735e536e88bf63e94c660bb8401d7
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-02 13:31:28 +0300

    3.15: Phonebook database, step3

--- part3/index.js
+++ part3/index.js
@@ -61,14 +61,11 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if(person) response.json(person)
-     else response.status(404).end()
+    Person.findById(request.params.id).then(person => response.json(person))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => response.json(person))
+    Person.findByIdAndRemove(request.params.id).then(result => response.status(204).end()).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
