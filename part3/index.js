4523bf312abc9cb4d94ae5e26c731ac518113351
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-02 14:49:45 +0300

    3.16: Phonebook database, step4

--- part3/index.js
+++ part3/index.js
@@ -36,6 +36,18 @@ let persons = [
     }
 ]
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+app.use(errorHandler)
+
 const generateId = () => {
     return Math.floor(Math.random() * 100000)
 }
@@ -60,11 +72,16 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => response.json(person))
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person) response.json(person)
+            else response.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id).then(result => response.status(204).end()).catch(error => next(error))
 })
 
