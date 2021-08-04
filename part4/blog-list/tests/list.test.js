b376ad5d67d5595d2617da751e2322c3c1b50433
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-04 13:13:37 +0300

    4.4: helper functions and unit tests, step2

--- part4/blog-list/tests/list.test.js
+++ part4/blog-list/tests/list.test.js
@@ -5,3 +5,58 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: "610a51bdf38b9b3930895504",
+      title: "Blog Example",
+      author: "FSO",
+      url: "https://fullstackopen.com/en/part4/",
+      likes: 4,
+      __v: 0
+    }
+  ]
+
+  const blogsList = [
+    {
+      _id: "610a51bdf38b9b3930895504",
+      title: "Blog Example 1",
+      author: "FSO",
+      url: "https://fullstackopen.com/en/part4/",
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: "610a51bdf38b9b3930895504",
+      title: "Blog Example 2",
+      author: "FSO",
+      url: "https://fullstackopen.com/en/part4/",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "610a51bdf38b9b3930895504",
+      title: "Blog Example 3",
+      author: "FSO",
+      url: "https://fullstackopen.com/en/part4/",
+      likes: 1,
+      __v: 0
+    }
+  ]
+
+  test('of one blog equals the likes of that blog', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(4)
+  })
+
+  test('of empty list to be 0', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('of list with multiple blogs to be calculated right', () => {
+    const result = listHelper.totalLikes(blogsList)
+    expect(result).toBe(8)
+  })
+})
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.length === 0 ?  0 : blogs.reduce((sum, item) => sum + item.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
