07aedbe2de0c5c682fb6a7fef2906ef04685e0f9
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-04 13:36:51 +0300

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -7,7 +7,14 @@ const totalLikes = blogs => {
   return blogs.length === 0 ?  0 : blogs.reduce((sum, item) => sum + item.likes, 0)
 }
 
+const favouriteBlog = blogs => {
+  if(blogs.length === 0) return 'no blogs in list!'
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  return blogs.filter(blog => blog.likes === maxLikes)[0]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
