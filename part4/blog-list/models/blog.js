00f121dfc728f745e296849391f137e6ce8f7da7
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-06 16:21:37 +0300

    4.15: bloglist expansion, step3

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -21,6 +21,10 @@ const blogSchema = new mongoose.Schema({
     minLenght: 5,
     default: 0
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
