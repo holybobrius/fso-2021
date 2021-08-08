d6a236237891e604154ae408241ad23d8c0a5967
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-08 18:20:42 +0300

    4.21*: bloglist expansion, step9

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -42,7 +42,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+  const blog = await Blog.findById(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if(!request.token || !decodedToken.id) return response.status(401).json({ error: 'invalid or missing token' })
+  const user = await User.findById(decodedToken.id)
+  logger.info(user.id.toString() === blog.user.toString())
+  if(user.id.toString() !== blog.user.toString()) return response.status(401).json({ error: 'wrong user!' })
+  blog.remove()
   response.status(204).end()
 })
 
