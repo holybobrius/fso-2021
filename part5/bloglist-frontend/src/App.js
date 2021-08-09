61b770fe8c1634de57f694c104eb95a5cffa4e36
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-09 14:39:48 +0300

    5.4*: bloglist frontend, step4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,12 +3,35 @@ import BlogsForm from './components/BlogsForm'
 import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [ notificationMessage, setNotificationMessage ] = useState(null)
+  const [ errorMessage, setErrorMessage ] = useState(null)
+  
+  const successfulNotifStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+
+  const errorNotifStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
 
   const handleUsername = event => {
     setUsername(event.target.value)
@@ -36,7 +59,10 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      console.log(user, 'Wrong credentials!')
+      setErrorMessage('Login failed!')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
   }
 
@@ -44,6 +70,10 @@ const App = () => {
     newBlog.user = user
     console.log(newBlog, newBlog.user.token)
     blogService.create(newBlog).then(addedBlog => setBlogs(blogs.concat(addedBlog)))
+    setNotificationMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
   }
 
   useEffect(() => {
@@ -63,6 +93,8 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={notificationMessage} styles={successfulNotifStyle}/>
+      <Notification message={errorMessage} styles={errorNotifStyle} />
       {user !== null 
         ? <BlogsForm blogs={blogs} handleClick={handleLogout} handleCreate={handleSubmit}/>
         : <LoginForm 
---
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,10 @@
+ const Notification = ({ message, styles }) => {
+  if(message === null) return null
+  return (
+      <div style={styles}>
+          {message}
+      </div>
+  )
+}
+
+export default Notification
