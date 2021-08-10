a52adff1b8d217cc3ceb6e461a1bdcee5a979b79
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-10 16:09:39 +0300

    5.9*: Blog list frontend, step9

--- part5/bloglist-frontend/src/components/BlogsForm.js
+++ part5/bloglist-frontend/src/components/BlogsForm.js
@@ -2,15 +2,17 @@ import Blog from "./Blog"
 import CreateForm from "./CreateForm"
 import Togglable from "./Togglable"
 
-const BlogsForm = ({ blogs, handleClick, handleCreate, handleUpdate }) => (
-  <div>
-    <Togglable buttonLabel='create new blog'><CreateForm handleCreate={handleCreate}/></Togglable>
-    <h2>blogs</h2>
-    {blogs.map(blog =>
-      <Blog key={blog.id} blog={blog}  handleUpdate={handleUpdate}/>
-    )}
-    <button onClick={handleClick}>logout</button>
-  </div>
-)
+const BlogsForm = ({ blogs, handleClick, handleCreate, handleUpdate }) => {
+  return (
+    <div>
+      <Togglable buttonLabel='create new blog'><CreateForm handleCreate={handleCreate}/></Togglable>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog}  handleUpdate={handleUpdate}/>
+      )}
+      <button onClick={handleClick}>logout</button>
+    </div>
+  )
+}
 
 export default BlogsForm
