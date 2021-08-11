47f790ec1da0e0d2bbbf511c96a9a267468568db
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-11 11:53:40 +0300

    5.14*: Blog list tests, step2

--- part5/bloglist-frontend/src/tests/Blog.test.js
+++ part5/bloglist-frontend/src/tests/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 describe('<Blog />', () => {
@@ -66,5 +66,47 @@ describe('<Blog />', () => {
     const likes = component.container.querySelector('.likes')
 
     expect(likes).toBe(null)
+  })
+
+  test('clicking the expand button renders blog url', () => {
+    const blog = {
+      title: 'blog for testing',
+      author: 'author for testing',
+      url: 'url for testing',
+      likes: 5,
+      user: { name: 'user for testing' }
+    }
+
+    const component = render(
+      <Blog blog={blog} />
+    )
+
+    const button = component.getByText('expand')
+    fireEvent.click(button)
+
+    const url = component.container.querySelector('.url')
+
+    expect(url).toHaveTextContent('url for testing')
+  })
+
+  test('clicking the expand button renders blog likes', () => {
+    const blog = {
+      title: 'blog for testing',
+      author: 'author for testing',
+      url: 'url for testing',
+      likes: 5,
+      user: { name: 'user for testing' }
+    }
+
+    const component = render(
+      <Blog blog={blog} />
+    )
+
+    const button = component.getByText('expand')
+    fireEvent.click(button)
+
+    const likes = component.container.querySelector('.likes')
+
+    expect(likes).toHaveTextContent('likes 5')
   })
 })
