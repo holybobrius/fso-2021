3c9d8861929d5c35803abc0ef3beddab0d2238f0
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-14 20:11:07 +0300

    6.12* Better anecdotes, step10

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdotesList from './components/AnecdotesList'
+import Filter from './components/Filter'
 
 const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdotesList />
       <AnecdoteForm />
     </div>
--- part6/redux-anecdotes/src/components/AnecdotesList.js
+++ part6/redux-anecdotes/src/components/AnecdotesList.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import anecdoteReducer, { voteFor } from '../reducers/anecdoteReducer'
+import { voteFor } from '../reducers/anecdoteReducer'
 import { sendConfirmation, endNotification } from '../reducers/notificationReducer'
 import Notification from './Notification'
 
 const AnecdotesList = () => {
   const anecdotes = useSelector(state => state.anecdotes).sort((a, b) => b.votes - a.votes)
   const notification = useSelector(state => state.notifications)
+  const filter = useSelector(state => state.filter)
+
   const dispatch = useDispatch()
 
   const vote = anecdote => {
@@ -14,20 +16,34 @@ const AnecdotesList = () => {
     dispatch(sendConfirmation(anecdote.content))
     setTimeout(() => dispatch(endNotification()), 5000)
   }
+
+  console.log(filter)
   return (
     <div>
       <Notification label={notification}/>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {filter === '' 
+        ? anecdotes.map(anecdote =>
+            <div key={anecdote.id}>
+              <div>
+                {anecdote.content}
+              </div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote)}>vote</button>
+              </div>
+            </div>
+          )
+        : anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase())).map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
+        )}
     </div>
   )
 }
