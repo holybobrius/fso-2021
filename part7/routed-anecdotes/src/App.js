4aa495adf394974d8b8a03c9290c180f6b1fee10
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2021-08-17 23:14:20 +0300

    7.6: anecdotes and hooks step3

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -68,16 +68,16 @@ const App = () => {
 }
 
 const CreateNew = (props) => {
-  const content = useField('content')
-  const author = useField('author')
-  const info = useField('info')
+  const { reset: resetContent, ...content } = useField('content')
+  const { reset: resetAuthor, ...author } = useField('author')
+  const { reset: resetInfo, ...info } = useField('info')
 
   const history = useHistory()
 
   const handleReset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   const handleSubmit = (e) => {
