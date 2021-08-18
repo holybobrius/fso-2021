c18345cee3c641acafdb12c1a28019becbfe588e
Ilya Povarov <ilya.povaroff@gmail.com>
2021-08-18 11:09:06 +0300

    7.8: ultimate hooks

---
+++ part7/ultimate-hooks/src/hooks/useResourse.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+  useEffect(() => axios.get(baseUrl).then(response => setResources(response.data)), [baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+
+export default useResource
